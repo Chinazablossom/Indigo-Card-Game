9e2763465e563adeaede233e2b557eaa11e620df
chinazaBlossom <chinazaBlossom7@gmail.com>
2024-07-18 18:41:11 +0100

    stage 3

--- Indigo Card Game/task/src/indigo/Main.kt
+++ Indigo Card Game/task/src/indigo/Main.kt
@@ -1,108 +1,177 @@
 package indigo
 
-import java.lang.NumberFormatException
 import kotlin.system.exitProcess
+import kotlin.text.StringBuilder
 
 
 fun main() {
-    IndigoCarGame()
+    `Indigo Card Game`()
 }
 
-
-class IndigoCarGame {
-    val cards = mutableSetOf<String>()
-    val cardsDeck = mutableSetOf<String>()
+class `Indigo Card Game` {
+    val playersCard = mutableSetOf<Card>()
+    val computersCard = mutableSetOf<Card>()
+    val cardsDeck = mutableSetOf<Card>()
+    val cardsOnTable = mutableSetOf<Card>()
+    var userWantsToPlayFirst = false
 
 
     init {
+        println(javaClass.simpleName)
         generateAllCards()
         prompt()
     }
 
     private fun prompt() {
-        do {
-            println("Choose an action (reset, shuffle, get, exit):")
-            val userAction = readln().trim()
-            when (userAction.lowercase()) {
-                "reset" -> resetCards()
-                "shuffle" -> shuffleCards()
-                "get" -> getCards()
-                "exit" -> exit()
+        println("Play first?")
+        val userWantsToPlayFirstInput = readln()
+        when (userWantsToPlayFirstInput) {
+            "yes" -> userWantsToPlayFirst = true
+            "no" -> userWantsToPlayFirst = false
+            else -> prompt()
+        }
+        Game()
+
+    }
+
+    private inner class Game() {
+
+        init {
+            getAllPlayersCard()
+            println("Initial cards on the table: ${cardsOnTable.joinToString(" ")}")
+
+            do {
+                println("\n${cardsOnTable.size} cards on the table, and the top card is ${cardsOnTable.last()}")
+                if (userWantsToPlayFirst) displayPlayerPrompt() else computerPrompt()
+
+            } while (cardsOnTable.size != 52)
+            exit()
+        }
+
+        private fun displayPlayerPrompt() {
+            println("Cards in hand: ${playersCard.printPlayerCard()}")
+            playerPrompt()
+        }
+
+        private fun playerPrompt() {
+            println("Choose a card to play (1-${playersCard.size}):")
+            val playersChoice = readlnOrNull()
+            processInput(playersChoice)
+        }
+
+        private fun computerPrompt() {
+
+            val randomNumsList = MutableList(computersCard.size) { it }
+            val randomNum = randomNumsList.random()
+            val randomComputerCard = computersCard.elementAt(randomNum)
+            randomNumsList.remove(randomNum)
+
+            computersCard.remove(randomComputerCard)
+            if (computersCard.isEmpty() && cardsDeck.isNotEmpty()) computersCard.resetCard()
+            cardsOnTable.add(randomComputerCard)
+            println("Computer plays $randomComputerCard")
+            userWantsToPlayFirst = true
+        }
+
+        private fun processInput(input: Any?) {
+            val inputToString = input as? String
+            when {
+                inputToString == null -> playerPrompt()
+                inputToString.equals("exit", true) -> exit()
+                inputToString.isBlank() -> playerPrompt()
                 else -> {
-                    println("Wrong action.")
+                    try {
+                        val inputToInt = inputToString.toInt()
+                        when (inputToInt) {
+                            in 1..6 -> {
+                                val card = playersCard.elementAtOrNull(inputToInt - 1)
+                                if (card != null) {
+                                    playersCard.remove(card)
+                                    if (playersCard.isEmpty() && cardsDeck.isNotEmpty()) playersCard.resetCard()
+                                    cardsOnTable.add(card)
+                                    userWantsToPlayFirst = false
+                                } else {
+                                    displayPlayerPrompt()
+                                }
+                            }
+
+                            else -> playerPrompt()
+                        }
+                    } catch (e: NumberFormatException) {
+                        playerPrompt()
+                    }
                 }
-
             }
+        }
 
-        } while (true)
-    }
+        private fun getAllPlayersCard() {
+            initialCardsOnTable()
+            playersCard.resetCard()
+            computersCard.resetCard()
+        }
 
-    private fun resetCards() {
-        cardsDeck.addAll(cards)
-        cards.clear()
-        println("Card deck is reset.")
-    }
-
-    private fun shuffleCards() {
-        cardsDeck.shuffled()
-        println("Card deck is shuffled.")
-    }
-
-    private fun getCards() {
-        try {
-            println("Number of cards:")
+        private fun MutableSet<Card>.resetCard() {
             var counter = 0
-            val newCards = mutableListOf<String>()
-
-            val numberOfCard = readln().toInt()
-            validateUserInputIsInt(numberOfCard)
-
-            loop@ for (i in cardsDeck) {
+            val newCards = mutableListOf<Card>()
+            val numberOfCard = 6
+            loop@ for (i in cardsDeck.shuffled()) {
                 counter++
-                cards.add(i)
+                this.add(i)
                 newCards.add(i)
                 if (counter == numberOfCard)
                     break@loop
             }
             counter = 0
-
-            cardsDeck.removeAll(cards)
-            println(newCards.shuffled().joinToString(" "))
-
+            cardsDeck.removeAll(newCards.toSet())
             newCards.clear()
-
-        } catch (e: NumberFormatException) {
-            println("Invalid number of cards.")
-        } catch (e: Exception) {
-            println(e.message)
         }
 
+        private fun MutableSet<Card>.printPlayerCard(): String {
+            val playersCard = StringBuilder()
+            this.forEachIndexed { index, card ->
+                playersCard.append("${index + 1})$card ")
+            }
+            return playersCard.toString()
+
+        }
 
     }
 
-    private fun generateAllCards(): MutableSet<String> {
+    private fun generateAllCards(): MutableSet<Card> {
         Ranks.values().forEach { rank ->
             Suits.values().forEach { suit ->
                 val card = Card(rank, suit)
-                cardsDeck.add("${card.rank.tag}${card.suit.icon}")
+                cardsDeck.add(card)
             }
         }
-        return cardsDeck.shuffled().toMutableSet()
+        return cardsDeck
+    }
+
+    private fun initialCardsOnTable(): MutableSet<Card> {
+        var count = 0
+        exit@ for (card in cardsDeck.shuffled()) {
+            count++
+            cardsOnTable.add(card)
+            if (count == 4) break@exit
+        }
+        cardsDeck.removeAll(cardsOnTable)
+        return cardsOnTable
 
     }
 
     private fun exit() {
-        println("Bye")
+        println(
+            if (cardsDeck.isEmpty())
+                "${cardsOnTable.size} cards on the table, and the top card is ${cardsOnTable.last()}\nGame Over"
+            else "Game Over"
+        )
         exitProcess(0)
     }
 
-    private fun validateUserInputIsInt(number: Any) {
-        if (number is Int && number !in 1..52 && cardsDeck.size <= 52) throw Exception("Invalid number of cards.")
-        if (number is Int && number > cardsDeck.size) throw Exception("The remaining cards are insufficient to meet the request.")
-        if (number !is Int) throw Exception("Invalid number of cards.")
-    }
 
-    inner class Card(val rank: Ranks, val suit: Suits)
+    inner class Card(val rank: Ranks, val suit: Suits) {
+        override fun toString() = "${rank.tag}${suit.icon}"
+    }
 
     enum class Suits(val icon: String) {
         DIAMONDS("♦"), HEARTS("♥"), SPADES("♠"), CLUBS("♣")
